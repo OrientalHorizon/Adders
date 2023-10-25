3048d7eb1bcff5c3d948d8965863949943d20a21
OrientalHorizon <iaknoip@outlook.com>
2023-10-25 14:01:58 +0800

    Begin multiplier

---
+++ booth-2.v
@@ -0,0 +1,12 @@
+module Booth(
+    input   [15:0]  a,
+    input   [15:0]  b,
+    input           clk,
+    output  [31:0]  mul
+);
+
+    wire [15:0] a_neg;
+    assign a_neg = ~a + 1;
+    for 
+
+endmodule
---
+++ multiplier.v
@@ -0,0 +1,8 @@
+module multiplier(
+    input [15:0] a,
+    input [15:0] b,
+    output [31:0] answer
+);
+
+
+endmodule
